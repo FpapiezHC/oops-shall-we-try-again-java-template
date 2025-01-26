78a1e2f9c70b22e742ea97521c904c2e15d06c5a
FpapiezHC <160175850+FpapiezHC@users.noreply.github.com>
2025-01-25 23:05:30 -0600

    Assigment3.Java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,37 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public int getInput() {
-    // Implement your function here
-  }
+  public int getInput(int lowerBound, int upperBound, String prompt, String errorMessage) {
+Scanner scanner = new Scanner(System.in);
+        int userInput;
+
+        while (true) {
+            System.out.println(prompt);
+            if (scanner.hasNextInt()) {
+                userInput = scanner.nextInt();
+                if (userInput >= lowerBound && userInput <= upperBound) {
+                    return userInput; 
+                } else {
+                    System.out.println(errorMessage);
+                }
+            } else {
+                System.out.println(errorMessage);
+                scanner.next(); 
+            }
+        }  }
 
   public static void main(String[] args) {
-    // Test your function here in a driver program
-  }
+    App app = new App();
+
+    int lowerBound = 1;
+    int upperBound = 100;
+    String prompt = "Please enter a value between 1 and 100:";
+    String errorMessage = "Your value is invalid. Try again.";
+
+    int userValue = app.getInput(lowerBound, upperBound, prompt, errorMessage);
+    System.out.println("The value chosen by the user is " + userValue);
 }
+ }
+
